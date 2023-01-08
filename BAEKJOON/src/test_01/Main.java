52ffab5c4dac4a048f02e3b92cb8926317ecc6d7
seratpfk <sera7057@naver.com>
2023-01-08 15:48:32 +0900

    2318

---
+++ BAEKJOON/src/test_01/Main.java
@@ -0,0 +1,19 @@
+package test_01;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int y = sc.nextInt();
+		
+		System.out.println(y - 543);
+		
+		sc.close();
+
+	}
+
+}
