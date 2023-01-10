e4263541824dbfa07cff465d7805d2c2846461b4
seratpfk <sera7057@naver.com>
2023-01-10 12:25:38 +0900

    0110

---
+++ BAEKJOON/src/test_01/Main11.java
@@ -0,0 +1,25 @@
+package test_01;
+
+import java.util.Scanner;
+
+public class Main11 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		int c = a * (b % 10);
+		int d = a * ((b % 100) / 10);
+		int e = a * (b / 100);
+		
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(a * b);
+
+	}
+
+}
