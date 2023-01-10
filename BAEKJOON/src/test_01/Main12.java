7207eaafa9a752e203d61de2683fc6aeed8c332a
seratpfk <sera7057@naver.com>
2023-01-10 13:09:03 +0900

    0110

--- BAEKJOON/src/test_01/Main12.java
+++ BAEKJOON/src/test_01/Main12.java
@@ -4,6 +4,7 @@ public class Main12 {
 
 	public static void main(String[] args) {
 		
+		// \를 출력하려면 \\를 입력해야 한다.
 		System.out.println("\\    /\\ ");
 		System.out.println(" )  ( ')");
 		System.out.println("(  /  ) ");
---
+++ BAEKJOON/src/test_01/Main13.java
@@ -0,0 +1,18 @@
+package test_01;
+
+public class Main13 {
+
+	public static void main(String[] args) {
+		
+		// "를 출력하려면 \"를 사용하여야 한다.
+		System.out.println("|\\_/|     ");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+		
+		System.out.println("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+
+	}
+
+}
