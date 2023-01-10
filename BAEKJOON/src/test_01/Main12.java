f327af0635a071422963681b93c440477e2251e4
seratpfk <sera7057@naver.com>
2023-01-10 12:40:20 +0900

    0110

---
+++ BAEKJOON/src/test_01/Main12.java
@@ -0,0 +1,14 @@
+package test_01;
+
+public class Main12 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\\    /\\ ");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  ) ");
+		System.out.println(" \\(__)| ");
+
+	}
+
+}
