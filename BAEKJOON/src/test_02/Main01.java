de8ee18d0cbf79071a0b24ffa271a2ee6acebd06
seratpfk <sera7057@naver.com>
2023-01-12 13:10:10 +0900

    0112

---
+++ BAEKJOON/src/test_02/Main01.java
@@ -0,0 +1,28 @@
+package test_02;
+
+import java.util.Scanner;
+
+public class Main01 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		if (A > B) {
+			System.out.println(">");
+		} 
+		
+		if (A < B) {
+			System.out.println("<");
+		}
+		
+		if (A == B) {
+			System.out.println("==");
+		}
+
+	}
+
+}
